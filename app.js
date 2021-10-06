10acc115b22592237dd999e239580960b26408d6
ritu2302 <regalritu2001@gmail.com>
2021-10-06 15:44:31 +0530

    primitive-data-types

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Hello world")
+console.warn("Hello warning")
+console.error("Hello error")
+console.log("Hello world")
+console.log(typeof "Hello world")
+
+//Numbers
+console.log(typeof 2)
+console.log(typeof -2)
+console.log(typeof -1.2)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
