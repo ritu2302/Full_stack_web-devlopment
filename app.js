173182e53f63f8fc3e8a4167741e8ce662114871
ritu2302 <regalritu2001@gmail.com>
2021-10-07 10:36:11 +0530

    variable

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-console.log("Hello world")
-console.warn("Hello warning")
-console.error("Hello error")
-console.log("Hello world")
-console.log(typeof "Hello world")
-
-//Numbers
-console.log(typeof 2)
-console.log(typeof -2)
-console.log(typeof -1.2)
-
-//boolean
-console.log(typeof true)
-console.log(typeof false)
+var arr =[ 1,2,3,4}
+    console.log(arr)
+    arr.push(5)
+    console.log(arr)
+    console.log(arr.length)
